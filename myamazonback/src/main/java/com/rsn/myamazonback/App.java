3a9d13a451eac23495f3c2d24c9ebfd2f2e773a9
Rishabh Soni <rsoni8088@gmail.com>
2018-04-15 15:35:36 +0530

    Online Project : My First commit to the Repo.

---
+++ myamazonback/src/main/java/com/rsn/myamazonback/App.java
@@ -0,0 +1,13 @@
+package com.rsn.myamazonback;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
