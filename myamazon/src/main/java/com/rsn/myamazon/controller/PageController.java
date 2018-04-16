cd145a5e16d7353828c8e36dfcd2c1e6aa2f008f
Rishabh Soni <rsoni8088@gmail.com>
2018-04-16 08:04:41 +0530

    Online Project : Created master page link with other pages as well.

--- myamazon/src/main/java/com/rsn/myamazon/controller/PageController.java
+++ myamazon/src/main/java/com/rsn/myamazon/controller/PageController.java
@@ -7,10 +7,30 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class PageController {
 
-	@RequestMapping(value = { "/", "hello", "index" })
+	@RequestMapping(value = { "home" })
 	public ModelAndView index() {
 		ModelAndView modelAndView = new ModelAndView("Page");
-		modelAndView.addObject("greeting", "Welcome to the Spring web MVC");
+		// to set the title of the page
+		modelAndView.addObject("title", "Home");
+		// this is used when we user click home
+		// this value is used in page.jsp along with the if condition.
+		modelAndView.addObject("userClickHome", true);
+		return modelAndView;
+	}
+
+	@RequestMapping(value = { "/about" })
+	public ModelAndView about() {
+		ModelAndView modelAndView = new ModelAndView("Page");
+		modelAndView.addObject("title", "About Us");
+		modelAndView.addObject("userClickAbout", true);
+		return modelAndView;
+	}
+
+	@RequestMapping(value = { "contact" })
+	public ModelAndView contact() {
+		ModelAndView modelAndView = new ModelAndView("Page");
+		modelAndView.addObject("title", "Contact Us");
+		modelAndView.addObject("userClickContact", true);
 		return modelAndView;
 	}
 }
